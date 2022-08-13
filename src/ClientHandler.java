d64297b6d6ae01bc708a1fbd7cba1ae683a0c46f
idlakshan <id.lakshan21@gmail.com>
2022-08-14 00:05:04 +0530

    Coding the server

---
+++ src/ClientHandler.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ClientHandler implements Runnable{
+
+    public static ArrayList<ClientHandler> clientHandlers=new ArrayList<>();
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+
+
+    @Override
+    public void run() {
+
+    }
+}
